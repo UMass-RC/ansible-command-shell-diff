753cb2b70cad87e83c58623a9bf08c3b8b048923
Simon Leary <simon.leary42@proton.me>
2024-06-20 17:44:31 +0000

    fix bug with passing on unsupported arguments

--- plugins/action/command.py
+++ plugins/action/command.py
@@ -3,8 +3,28 @@
 
 from __future__ import annotations
 
+import os
+import shutil
+import tempfile
+
 from ansible.plugins.action import ActionBase
 from ansible.utils.vars import merge_hash
+from ansible.parsing.splitter import split_args
+
+
+def _create_copy_or_empty_tempfile(path: str):
+    """
+    Create a tempfile containing a copy of the file at `path`.
+    If `path` does not point to a file, create an empty tempfile.
+    """
+    fd, tempfile_path = tempfile.mkstemp(dir=C.DEFAULT_LOCAL_TMP)
+    if os.path.isfile(path):
+        try:
+            shutil.copy(path, tempfile_path)
+        except Exception as err:
+            os.remove(tempfile_path)
+            raise Exception(err)
+    return tempfile_path
 
 
 class ActionModule(ActionBase):
@@ -13,6 +33,14 @@ class ActionModule(ActionBase):
         self._supports_async = True
         results = super(ActionModule, self).run(tmp, task_vars)
         del tmp  # tmp no longer has any effect
+
+        # the normal command module has presumably this same logic buried somewhere else in the source code
+        if "cmd" in self._task.args:
+            if self._task.args.get("_uses_shell", False):
+                self._task.args["_raw_params"] = self._task.args["cmd"]
+            else:
+                self._task.args["argv"] = split_args(self._task.args["cmd"])
+            del self._task.args["cmd"]
 
         wrap_async = self._task.async_val and not self._connection.has_native_async
         results = merge_hash(results, self._execute_module(module_name='unity.command_shell_diff.command', task_vars=task_vars, wrap_async=wrap_async))
